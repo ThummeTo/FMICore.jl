994474af6141bcaee072deded20c804340805c6a
TT <tobias.thummerer@informatik.uni-augsburg.de>
2022-04-21 19:15:24 +0200

    minor fix

--- src/FMI3.jl
+++ src/FMI3.jl
@@ -165,84 +165,6 @@ FMU states:     $(c.x)"
 )
 
 """
-A mutable struct representing the excution configuration of a FMU.
-For FMUs that have issues with calls like `fmi2Reset` or `fmi2FreeInstance`, this is pretty useful.
-"""
-mutable struct FMU3ExecutionConfiguration 
-    terminate::Bool     # call fmi3Terminate before every training step / simulation
-    reset::Bool         # call fmi3Reset before every training step / simulation
-    setup::Bool         # call setup functions before every training step / simulation
-    instantiate::Bool   # call fmi3Instantiate before every training step / simulation
-    freeInstance::Bool  # call fmi3FreeInstance after every training step / simulation
-
-    handleStateEvents::Bool                 # handle state events during simulation/training
-    handleTimeEvents::Bool                  # handle time events during simulation/training
-
-    assertOnError::Bool                     # wheter an exception is thrown if a fmi3XXX-command fails (>= fmi3StatusError)
-    assertOnWarning::Bool                   # wheter an exception is thrown if a fmi3XXX-command warns (>= fmi3StatusWarning)
-
-    autoTimeShift::Bool                     # wheter to shift all time-related functions for simulation intervals not starting at 0.0
-
-    sensealg                                # algorithm for sensitivity estimation over solve call
-    useComponentShadow::Bool                # whether FMU outputs/derivatives/jacobians should be cached for frule/rrule (useful for ForwardDiff)
-    rootSearchInterpolationPoints::UInt     # number of root search interpolation points
-    inPlace::Bool                           # whether faster in-place-fx should be used
-    useVectorCallbacks::Bool                # whether to vector (faster) or scalar (slower) callbacks
-
-    maxNewDiscreteStateCalls::UInt          # max calls for fmi3NewDiscreteStates before throwing an exception
-
-    function FMU3ExecutionConfiguration()
-        inst = new()
-
-        inst.terminate = true 
-        inst.reset = true
-        inst.setup = true
-        inst.instantiate = false 
-        inst.freeInstance = false
-
-        inst.handleStateEvents = true
-        inst.handleTimeEvents = true
-
-        inst.assertOnError = false
-        inst.assertOnWarning = false
-
-        inst.autoTimeShift = true
-
-        inst.sensealg = nothing # auto
-        inst.useComponentShadow = false
-        inst.rootSearchInterpolationPoints = 100
-        inst.inPlace = true
-        inst.useVectorCallbacks = true
-
-        inst.maxNewDiscreteStateCalls = 100
-
-        return inst 
-    end
-end
-
-# default for a "healthy" FMU - this is the fastetst 
-FMU_EXECUTION_CONFIGURATION_RESET = FMU3ExecutionConfiguration()
-FMU_EXECUTION_CONFIGURATION_RESET.terminate = true
-FMU_EXECUTION_CONFIGURATION_RESET.reset = true
-FMU_EXECUTION_CONFIGURATION_RESET.instantiate = false
-FMU_EXECUTION_CONFIGURATION_RESET.freeInstance = false
-
-# if your FMU has a problem with "fmi3Reset" - this is default
-FMU_EXECUTION_CONFIGURATION_NO_RESET = FMU3ExecutionConfiguration() 
-FMU_EXECUTION_CONFIGURATION_NO_RESET.terminate = false
-FMU_EXECUTION_CONFIGURATION_NO_RESET.reset = false
-FMU_EXECUTION_CONFIGURATION_NO_RESET.instantiate = true
-FMU_EXECUTION_CONFIGURATION_NO_RESET.freeInstance = true
-
-# if your FMU has a problem with "fmi3Reset" and "fmi3FreeInstance" - this is for weak FMUs (but slower)
-FMU_EXECUTION_CONFIGURATION_NO_FREEING = FMU3ExecutionConfiguration() 
-FMU_EXECUTION_CONFIGURATION_NO_FREEING.terminate = false
-FMU_EXECUTION_CONFIGURATION_NO_FREEING.reset = false
-FMU_EXECUTION_CONFIGURATION_NO_FREEING.instantiate = true
-FMU_EXECUTION_CONFIGURATION_NO_FREEING.freeInstance = false
-
-
-"""
 Source: FMISpec3.0, Version D5ef1c1: 2.2.1. Header Files and Naming of Functions
 
 The mutable struct representing an FMU in the FMI 3.0 Standard.
@@ -345,7 +267,7 @@ mutable struct FMU3 <: FMU
     zipPath::String
 
     # execution configuration
-    executionConfig::FMU3ExecutionConfiguration
+    executionConfig::FMUExecutionConfiguration
     hasStateEvents::Union{Bool, Nothing} 
     hasTimeEvents::Union{Bool, Nothing} 
 
