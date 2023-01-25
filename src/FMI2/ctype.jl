b5b980bf21244b819614732b0a62d596aed19afe
Manuel <manuelbb@mail.uni-paderborn.de>
2023-01-25 14:32:08 +0100

    Update docstring of `DisplayUnit` in src/FMI2/ctype.jl
    Co-authored-by: JoKircher <79573182+JoKircher@users.noreply.github.com>

--- src/FMI2/ctype.jl
+++ src/FMI2/ctype.jl
@@ -584,11 +584,13 @@ function Base.show(io::IO, unit::BaseUnit)
 
     print(io,"BaseUnit( $(unit_str) )")
 end
+
 """
+Source: FMISpec2.0.3[p.35]: 2.2.2 Definition of Units (UnitDefinitions)
+
     DisplayUnit(name, factor=1.0, offset=0.0)
 
 Type for the optional “DisplayUnit” field(s) of an `fmi2Unit`.
-Source: FMISpec2.0.3: 2.2.2 Definition of Units (UnitDefinitions)
 """
 mutable struct DisplayUnit
     # mandatory
