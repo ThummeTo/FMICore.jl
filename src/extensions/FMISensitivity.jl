72be71585e322b594434b0db92ef0da66738edea
TT <tobias.thummerer@informatik.uni-augsburg.de>
2023-10-07 11:00:13 +0200

    minor change

--- src/extensions/FMISensitivity.jl
+++ src/extensions/FMISensitivity.jl
@@ -6,51 +6,10 @@
 import .FMISensitivity.ForwardDiff 
 import .FMISensitivity.ReverseDiff
 
-# function getDerivatives!(c::FMU2Component, dx::AbstractArray{<:ForwardDiff.Dual})
-#     dx_tmp = ForwardDiff.value.(dx)
-#     c.fmu.isZeroState || fmi2GetDerivatives!(c, dx_tmp)
-
-#     for i = 1:length(dx)
-#         dx[i].value = dx_tmp[i]
-#     end
-    
-#     return nothing
-# end
-
-# function getDerivatives!(c::FMU2Component, dx::ReverseDiff.TrackedArray)
-#     dx_tmp = zeros(fmi2Real, length(dx))
-#     fmi2GetDerivatives!(c, dx_tmp)
-
-#     for i in 1:length(dx)
-#         dx[i].value = dx_tmp[i]
-#     end  
-
-#     return nothing
-# end
-
-# function getOutputs!(c::FMU2Component, y::AbstractArray{<:ForwardDiff.Dual}, y_refs)
-#     y_tmp = collect(ForwardDiff.value(e) for e in y)
-#     fmi2GetReal!(c, y_refs, y_tmp)
-    
-#     for i = 1:length(y)
-#         y[i].value = y_tmp[i]
-#     end
-
-#     return nothing
-# end
-
-# function getOutputs!(c::FMU2Component, y::ReverseDiff.TrackedArray, y_refs)
-#     @assert false, "getOutputs!() not implemented (yet) for ReverseDiff, please open an issue with MWE."
-
-#     return nothing
-# end
-
 ###########
 
 import .FMISensitivity.SciMLSensitivity.ForwardDiff
 import .FMISensitivity.SciMLSensitivity.ReverseDiff
-
-import FMICore: isdual, istracked, fd_eltypes, sense_set!, undual, untrack, unsense
 
 # check if scalar/vector is ForwardDiff.Dual
 function isdual(e::ForwardDiff.Dual{T, V, N}) where {T, V, N}
@@ -67,33 +26,8 @@ end
 function istracked(e::AbstractVector{<:ReverseDiff.TrackedReal}) 
     return true
 end
-
-# check types (Tag, Variable, Number) of ForwardDiff.Dual scalar/vector
-function fd_eltypes(e::ForwardDiff.Dual{T, V, N}) where {T, V, N}
-    return (T, V, N)
-end
-function fd_eltypes(e::AbstractVector{<:ForwardDiff.Dual{T, V, N}}) where {T, V, N}
-    return (T, V, N)
-end
-
-# overwrites a ForwardDiff.Dual in-place 
-# inheritates partials
-function sense_set!(dst::AbstractArray{<:Float64}, src::AbstractArray{<:ForwardDiff.Dual})
-    dst[:] = collect(ForwardDiff.value(e) for e in src)
-    return nothing
-end
-function sense_set!(dst::AbstractArray{<:ForwardDiff.Dual}, src::AbstractArray{<:Float64})
-    T, V, N = fd_eltypes(dst)
-    dst[:] = collect(ForwardDiff.Dual{T, V, N}(V(src[i]), ForwardDiff.partials(dst[i])    ) for i in 1:length(dst))
-    return nothing
-end
-function sense_set!(dst::ReverseDiff.TrackedArray, src::AbstractArray{<:ForwardDiff.Dual})
-    dst[:] = collect(ReverseDiff.TrackedReal(ReverseDiff.value(src[i]), 0.0    ) for i in 1:length(dst))
-    return nothing
-end
-function sense_set!(dst::AbstractArray{<:Float64}, src::ReverseDiff.TrackedArray)
-    dst[:] = collect(ReverseDiff.value(e) for e in src)
-    return nothing
+function istracked(e::ReverseDiff.TrackedArray) 
+    return true
 end
 
 # makes Reals from ForwardDiff.Dual scalar/vector
