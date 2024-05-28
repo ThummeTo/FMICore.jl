5f65962344f15f5f8d43b2ca619642c5da94cd05
halentin <va.hoepfner@gmail.com>
2024-05-28 15:44:17 +0000

    Add devcontainer and Dependencies

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,9 +5,11 @@
 
 using FMICore
 using Test
+using Downloads, ZipFile
 
 @testset "FMICore.jl" begin
     @testset "FMI2" begin
-        # [ToDo]
+        println("Hallo :)")
+        dl = Downloads.download("https://github.com/ThummeTo/FMIZoo.jl/raw/main/models/bin/Dymola/2023x/2.0/BouncingBallGravitySwitch1D.fmu")
     end
 end
