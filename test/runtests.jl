1d938fd1f0bb42eb3f6df70a0b9d0ac43e6f892f
halentin <va.hoepfner@gmail.com>
2024-05-31 08:11:35 +0000

    s

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,6 +10,12 @@ using Downloads, ZipFile
 @testset "FMICore.jl" begin
     @testset "FMI2" begin
         println("Hallo :)")
-        dl = Downloads.download("https://github.com/ThummeTo/FMIZoo.jl/raw/main/models/bin/Dymola/2023x/2.0/BouncingBallGravitySwitch1D.fmu")
+        # dl = Downloads.download("https://github.com/ThummeTo/FMIZoo.jl/raw/main/models/bin/Dymola/2023x/2.0/BouncingBallGravitySwitch1D.fmu")
+        # Download
+        # Unzip
+        # OS-Distinction
+            # Dynamic Linking
+        # Tests
+        
     end
 end
