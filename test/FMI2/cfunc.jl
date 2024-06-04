007b34007b67a3347e8d488bc9333f1007fede2c
halentin <va.hoepfner@gmail.com>
2024-06-04 14:21:53 +0000

    Working Tests for Instantiate, Free, GetVersion and GetTypesPlatform

--- test/FMI2/cfunc.jl
+++ test/FMI2/cfunc.jl
@@ -64,7 +64,48 @@ end
 
 binarypath = joinpath(binarypath, "BouncingBallGravitySwitch1D")
 lib = dlopen(binarypath)
-cInstantiate                  = dlsym(lib, :fmi2Instantiate)
+
+cInstantiate = dlsym(lib, :fmi2Instantiate)
+function instantiate_args()
+    # TODO get guid and Name from XML
+    guidStr = "{3c564ab6-a92a-48ca-ae7d-591f819b1d93}"
+    ptrAllocateMemory = C_NULL
+    ptrFreeMemory = C_NULL
+    ptrStepFinished = C_NULL # ToDo
+    ptrComponentEnvironment = C_NULL
+    ptrLogger = C_NULL
+    fmi2TypeModelExchange
+    instanceName = "BouncingBallGravitySwitch1D"
+    callbackFunctions = fmi2CallbackFunctions(ptrLogger, ptrAllocateMemory, ptrFreeMemory, ptrStepFinished, ptrComponentEnvironment)
+    resourcelocation = string("file:///", path)
+    resourcelocation = joinpath(resourcelocation, "resources")
+    resourcelocation = replace(resourcelocation, "\\" => "/")
+    type = fmi2TypeModelExchange
+    visible = fmi2Boolean(true)
+    loggingon = fmi2Boolean(true)
+
+    (pointer(instanceName),type, pointer(guidStr), pointer(resourcelocation), Ptr{fmi2CallbackFunctions}(pointer_from_objref(callbackFunctions)), visible, loggingon)
+end
+compAddr = fmi2Instantiate(cInstantiate, instantiate_args()...)
+@test compAddr != Ptr{Cvoid}(C_NULL)
+component = fmi2Component(compAddr)
+println(fmi2Component(compAddr))
+cFree = dlsym(lib, :fmi2FreeInstance)
+fmi2FreeInstance(cFree, component)
+# fmi2FreeInstance(cFree, component)
+
+## fmi2GetTypesPlatform
+cGetTypesPlatform = dlsym(lib, :fmi2GetTypesPlatform)
+test = fmi2GetTypesPlatform(cGetTypesPlatform)
+@test unsafe_string(test) == "default"
+
+## fmi2GetVersion TODO get Version from modelDescription.xml
+vers = fmi2GetVersion(dlsym(lib, :fmi2GetVersion))
+@test unsafe_string(vers) == "2.0"
+
+
+# Callback Functions werden in FMIImport.jl/c.jl definiert
+
 # Download
 # Unzip
 # OS-Distinction
