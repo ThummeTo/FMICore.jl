c9a7bbd8eaf91c95e7416b53dfbe77a8ffad9db1
Valentin Höpfner <valentin.hoepfner@uni-a.de>
2024-06-17 16:09:51 +0200

    replace OS-Check Test with warning and skip

--- test/FMI2/cfunc.jl
+++ test/FMI2/cfunc.jl
@@ -9,7 +9,6 @@ include.(["utils.jl", "ME.jl", "CS.jl", "generic.jl"])
 
 
 binarypath, fmu_path, cblibpath = get_os_binaries()
-@test binarypath != ""
 if binarypath != ""
     lib = dlopen(binarypath)
     # Missing Tests for fmi2<Set, Get><Boolean, String> because the FMU we are testing with doesnt variables of these types
@@ -17,5 +16,6 @@ if binarypath != ""
     @testset "Generic Functions in CS Mode" test_generic(lib,cblibpath, fmi2TypeCoSimulation)
     @testset "ME-specific Functions" test_ME(lib, cblibpath)
     @testset "CS-specific Functions" test_CS(lib, cblibpath)
-    
+else
+    @warn "No valid FMU binaries found for this OS. Skipping tests."
 end
